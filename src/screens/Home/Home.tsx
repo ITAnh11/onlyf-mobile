543df92d15edbff4accbb1507726ceeb77c669af
DAOGIANGAN <125682086+DAOGIANGAN@users.noreply.github.com>
2025-04-11 02:43:12 +0700

    feat(post): hoàn thiện chức năng xem bài đăng

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StatusBar, Image, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StatusBar, Image, Button, FlatList } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationProp } from '@react-navigation/native';
 import { styles } from './styles';
@@ -10,20 +10,110 @@ import ProfileApi from '../../networking/profile.api';
 import Posting from './components/Posting';
 import { useCameraPermissions } from 'expo-camera';
 import { useMediaLibraryPermissions } from 'expo-image-picker';
+import { Dimensions } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './Global/PostStore';
+import { addPost, addPosts, setHasMore, setNextCursor } from './Global/PostSlice';
+import PostView from './components/PostView';
+import ProfileService from '../../services/profile.service';
+
 
 type Props = {
   navigation: NavigationProp<any>;
 };
 
 const Home: React.FC<Props> = ({ navigation }) => {  
-  const [compressedUri, setCompressedUri] = useState<string | null>(null);
+  const [compressedUri, setCompressedUri] = useState<string | null>(null); 
+  const { height } = Dimensions.get('window');
+  const avatar = ProfileService.getAvatar(); // Lấy avatar từ ProfileService
+  // State để theo dõi trạng thái quyền
   const [permission, requestPermission] = useCameraPermissions();
   const [permission_library, requestPermission_library] = useMediaLibraryPermissions();
   const [permissionsGranted, setPermissionsGranted] = useState(false); // State để theo dõi trạng thái quyền
 
+
+  // State để theo dõi số lượng các bài post
+  const [loading, setLoading] = useState(false);
+
+  const dispatch = useDispatch(); // Khởi tạo dispatch từ Redux
+  const danhSach = useSelector((state: RootState) => state.postManager.danhSach);
+  const nextCursor = useSelector((state: RootState) => state.postManager.nextCursor);
+  const hasMore = useSelector((state: RootState) => state.postManager.hasdmore);
+  
+  
+  //useEffect để thêm home vào data array khi component mount
+  useEffect(() => {
+    dispatch(addPost({ id: 'home', caption: 'Home', urlPublicImage: '', createdAt: '', user: { id: '', profile: { name: '', urlPublicAvatar: '' } } })); // Thêm home vào danh sách bài post
+  }, []);
+
+  useEffect(() => {
+    console.log("danhSach đã thay đổi:", danhSach, "\n");
+  }, [danhSach]);
+
+
+  //useEffect để khỏi tạo các bài post đầu tiên
+  useEffect(() => {
+    const fetchCards = async () => {
+      if (loading) return; // Ngăn việc gọi API nhiều lần khi đang tải
+      setLoading(true); // Bắt đầu trạng thái tải
+      const accessToken = await TokenService.getAccessToken();
+      try {
+        const response = await apiClient.get(`/post/get-posts/`, {
+          params: {
+            limit: 4, // Số lượng bài post muốn lấy
+          },
+          headers: {
+            Authorization: `Bearer ${accessToken}` // Thêm access token vào header
+          }
+        });
+        console.log("response", response.data.posts); // Kiểm tra dữ liệu trả về từ API
+        dispatch(addPosts(response.data.posts)); // Cập nhật state `data` với bài post mới
+        dispatch(setNextCursor(response.data.nextCursor)); // Cập nhật con trỏ tiếp theo
+        dispatch(setHasMore(response.data.hasMore)); // Cập nhật trạng thái có thêm bài post hay khôngr
+      } catch (error) {
+        console.error("Lỗi khi gọi API:", error); // Xử lý lỗi nếu có
+      } finally {
+        setLoading(false); // Kết thúc trạng thái tải
+      }
+    }
+    fetchCards(); // Gọi hàm fetchCards khi component mount
+  }, []);
+
+ // Hàm gọi API để lấy danh sách bài post
+  const fetchCards = async () => {
+      if (nextCursor !== null && hasMore) { // Kiểm tra xem có con trỏ tiếp theo và còn bài post để tải không
+          if (loading) return; // Ngăn việc gọi API nhiều lần khi đang tải
+          setLoading(true); // Bắt đầu trạng thái tải
+          const accessToken = await TokenService.getAccessToken();
+          try {
+            const response = await apiClient.get(`/post/get-posts/`, {
+              params: {
+                limit: 4, // Số lượng bài post muốn lấy
+                cursor: nextCursor // Tham số con trỏ để phân trang
+              },
+              headers: {
+                Authorization: `Bearer ${accessToken}` // Thêm access token vào header
+              }
+            });
+            console.log("response", response.data.posts); // Kiểm tra dữ liệu trả về từ API
+            dispatch(addPosts(response.data.posts)); // Cập nhật state `data` với bài post mới
+            dispatch(setNextCursor(response.data.nextCursor)); // Cập nhật con trỏ tiếp theo
+            dispatch(setHasMore(response.data.hasMore)); // Cập nhật trạng thái có thêm bài post hay khôngr
+          } catch (error) {
+            console.error("Lỗi khi gọi API:", error); // Xử lý lỗi nếu có
+          } finally {
+            setLoading(false); // Kết thúc trạng thái tải
+          }
+      } else { return; } // Nếu không còn bài post nào để tải thêm, không làm gì cả
+  };
+
+  
+  //Ham xử lý cấp quyền camera và thư viện ảnh
   const checkPermissions = async () => {
     let cameraGranted = permission?.granted;
     let libraryGranted = permission_library?.granted;
+
+
 
     if (!cameraGranted) {
       const cameraPermission = await requestPermission();
@@ -73,51 +163,101 @@ const Home: React.FC<Props> = ({ navigation }) => {
     alert("Đăng xuất thành công!");
   }
 
+  //lấy thông tin người dùng khi component mount
   useEffect(() => {
-    ProfileApi.getProfile();
+    ProfileApi.getProfile(); // Lấy thông tin người dùng từ ProfileApi
   }, []);
 
+  // Kiểm tra quyền camera và thư viện ảnh khi component mount
   useEffect(() => {
     checkPermissions();
   }, []);
 
+  type UserProfile = {
+    name: string;
+    urlPublicAvatar: string;
+  };
+
+  type User = {
+    id: string;
+    profile: UserProfile;
+  };
+
+  type PostItem = {
+    id: string;
+    caption: string;
+    urlPublicImage: string;
+    createdAt: string;
+    user: User;
+  };
+
+  const renderItem = ({ item }: { item: PostItem }) => {
+    if (item.id === 'home') {
+      return (
+        <View>
+          {/* Nội dung trang Home giống như bạn viết ở trên */}
+          <SafeAreaView style={{ flexDirection: "column",height: height,flex: 1 }}>
+            {permissionsGranted ? (
+              compressedUri ? (
+                <View style={styles.camera_container}>
+                  <Posting compressedUri={compressedUri} setCompressedUri={setCompressedUri} />
+                </View>
+              ) : (
+                <View style={styles.camera_container}>
+                  <CustomCamera onPhotoTaken={setCompressedUri} />
+                </View>
+              )
+            ) : (
+              <View style={styles.camera_container}>
+                <Text>Đang chờ cấp quyền...</Text>
+              </View>
+            )}
+          </SafeAreaView>
+        </View>
+      );
+    } else {
+      return (
+        <PostView post={item} /> // Hiển thị bài post
+      );
+    }
+
+  };
+  
   return (
-    <SafeAreaProvider style={styles.safeArea_style}>
+    <SafeAreaProvider>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      <SafeAreaView style={{ flexDirection: "column", flex: 1 }}>
-        {/* Các nút chức năng */}
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#111111', height: height }}>
         <View style={styles.list_button}>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Profile")}>
-            <Image source={require("../../assets/user.png")} resizeMode="contain" style={{ width: 40, height: 40 }} />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Friend")}>
-            <Image source={require("../../assets/add-friend.png")} resizeMode="contain" style={{ width: 40, height: 40 }} />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Message")}>
-            <Image source={require("../../assets/message.png")} resizeMode="contain" style={{ width: 40, height: 40 }} />
-          </TouchableOpacity>
-          <Button title="Đăng xuất" onPress={handleLogout} color="#FF0000" />
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Profile")}>
+              <Image source={require("../../assets/user.png")} resizeMode="contain" style={{ width: 40, height: 40 }} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Friend")}>
+              <Image source={require("../../assets/add-friend.png")} resizeMode="contain" style={{ width: 40, height: 40 }} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Message")}>
+              <Image source={require("../../assets/message.png")} resizeMode="contain" style={{ width: 40, height: 40 }} />
+            </TouchableOpacity>
+            <Button title="Đăng xuất" onPress={handleLogout} color="#FF0000" />
         </View>
 
-        {/* Camera hoặc ảnh đã chụp */}
-        {permissionsGranted ? (
-          compressedUri ? (
-            <View style={styles.camera_container}>
-              <Posting compressedUri={compressedUri} setCompressedUri={setCompressedUri} />
-            </View>
-          ) : (
-            <View style={styles.camera_container}>
-              <CustomCamera onPhotoTaken={setCompressedUri} />
-            </View>
-          )
-        ) : (
-          <View style={styles.camera_container}>
-            <Text>Đang chờ cấp quyền...</Text>
-          </View>
-        )}
+        <FlatList
+          data={danhSach}
+          renderItem={({ item }) => renderItem({ item })}
+          keyExtractor={(item, index) => item.id === 'home' ? `home-${index}` : item.id.toString()}
+          contentContainerStyle={{ flexGrow: 1 }} // Đảm bảo FlatList chiếm toàn bộ không gian
+          showsVerticalScrollIndicator={false} // Ẩn thanh cuộn dọc
+          onEndReached={fetchCards}
+          onEndReachedThreshold={0.3}
+          pagingEnabled={true} // Bật chế độ cuộn trang
+          getItemLayout={(data, index) => (
+            { length: height, offset: height * index, index } // Cung cấp chiều cao của mỗi mục
+          )}
+          />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 
 export default Home;
+
+
---
+++ src/screens/Home/components/PostView.tsx
@@ -0,0 +1,139 @@
+import { View, Text, ImageBackground, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Message from '../../Message';
+
+
+type UserProfile = {
+  name: string;
+  urlPublicAvatar: string;
+};
+
+type User = {
+  id: string;
+  profile: UserProfile;
+};
+
+type PostItem = {
+  id: string;
+  caption: string;
+  urlPublicImage: string;
+  createdAt: string;
+  user: User;
+};
+type PostViewProps = {
+  post: PostItem;
+};
+
+const PostView = ({ post }: PostViewProps) => {
+  return (
+    <SafeAreaView style={{ flexDirection: "column", height: Dimensions.get('window').height, flex: 1 }}>
+      <View style={styles.Post_container}>
+        <ImageBackground source={{ uri: post.urlPublicImage }} style={styles.Image}>
+           <Text style={styles.Caption}>{post.caption}</Text>
+        </ImageBackground>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10, alignItems: 'center', marginBottom: 20 }}>
+        <ImageBackground source={{ uri: post.user.profile.urlPublicAvatar }} style={styles.User_avatar} />
+        <Text style={styles.User_name}>{post.user.profile.name}</Text>
+      </View>
+      <View style={styles.Message_container}>
+        <Text style={styles.Message}>Gửi tin nhắn...</Text>
+        <View style={styles.Emoji_container}>
+          <Text style={styles.Emoji}>❤️</Text>
+          <Text style={styles.Emoji}>🔥</Text>
+          <Text style={styles.Emoji}>😂</Text>
+        </View>
+      </View>
+      <View style={styles.Button_container}>
+        <TouchableOpacity >
+          <ImageBackground source={require("../../../assets/All_post_icon.png")} resizeMode="contain" style={{ width: 30, height: 30 }} />
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <View style={{ width: 60, height: 60, backgroundColor: '#EAA905', borderRadius: 50, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{ width: 53, height: 53, backgroundColor: 'white', borderRadius: 50, alignItems: 'center', justifyContent: 'center',borderWidth: 3, borderColor: "black" }}/>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <ImageBackground source={require("../../../assets/option.png")} resizeMode="contain" style={{ width: 30, height: 30 }} />
+        </TouchableOpacity>
+      </View>
+
+    </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+  Post_container: { 
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    marginTop: 80,
+  },
+  Image: {
+    width: '100%',
+    aspectRatio: 1,
+    borderRadius: 60,
+    overflow: 'hidden',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  Caption: {
+    marginTop: 10,
+    marginHorizontal: 20,
+    padding: 10,
+    borderRadius: 30,
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    fontSize: 16,
+    color: 'white',
+    fontWeight: "bold",
+    height: 45,
+    marginBottom:20,
+  },
+  User_name: {
+    fontSize: 18,
+    color: '#e0e0e0',
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
+  User_avatar: {
+    width: 30,
+    height: 30,
+    borderRadius: 15, 
+    backgroundColor: 'white',
+  },
+  Message_container:{
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: "#333333",
+    height: 50,
+    width: '90%',
+    marginLeft: "5%",
+    marginTop: "20%",
+    borderRadius: 30,
+  },
+  Message: {
+    fontSize: 18,
+    color: '#bfbebd',
+    marginLeft: 10,
+    fontWeight: 'bold',
+    backgroundColor: "#333333",
+  },
+  Emoji_container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '40%',
+  },
+  Emoji: {
+    fontSize: 24,
+  },
+  Button_container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 25,
+  },
+});
+export default PostView
