65e216d08ce7be15fb2246650a7d62b574dd61a8
DAOGIANGAN <125682086+DAOGIANGAN@users.noreply.github.com>
2025-04-14 00:32:34 +0700

    feat(Font): thêm file kích thước cỡ chữ chung cho cả App

---
+++ src/constants/Font.ts
@@ -0,0 +1,13 @@
+const Font = {
+    heading1: 32,
+    heading2: 28,
+    heading3: 24,
+    heading4: 20,
+    title: 18,
+    subtitle: 16,
+    body: 14,
+    caption: 12,
+    small: 10,
+}
+
+export default Font;
