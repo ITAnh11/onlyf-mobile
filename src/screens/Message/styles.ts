cd8635774a5eee9b69f88bade055d7d3f094710e
dtpthanh <22021167@vnu.edu.vn>
2025-04-25 02:22:13 +0700

    feat(message): Thêm chức năng nhắn tin

---
+++ src/screens/Message/styles.ts
@@ -0,0 +1,69 @@
+import { StyleSheet } from "react-native";
+import Colors from "../../constants/Color";
+
+const styles = StyleSheet.create({
+  message: {
+    flex: 1,
+    backgroundColor: Colors.primary_background,
+  },
+  container: {
+    paddingBottom: 30,
+    backgroundColor: Colors.primary_background,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 60,
+    paddingHorizontal: 16,
+    borderBottomWidth: 1,
+    backgroundColor: Colors.primary_background,
+    marginTop: '10%',
+  },
+  backButton: {
+    fontSize: 26,
+    color: Colors.white_button,
+    zIndex: 2,
+  },
+  title: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginLeft: -26,
+    color: Colors.primary_text,
+  },
+  chatItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    borderWidth: 3,
+    borderColor: Colors.border_avt2,
+    marginRight: 20,
+  },
+  name: {
+    fontSize: 16,
+    color: Colors.primary_text,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  lastMessage: {
+    fontSize: 13,
+    color: '#888',
+  },  
+  loadingText: {
+    textAlign: 'center',
+    padding: 20,
+  },
+  errorText: {
+    color: 'red',
+    textAlign: 'center',
+    padding: 20,
+  },  
+});
+
+export default styles;
