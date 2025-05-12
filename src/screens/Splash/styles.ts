6a8e9f41ca2c8a920ce6f025487afaa457f2ce7f
ITAnh11 <buianhkc112004@gmail.com>
2025-05-13 01:56:07 +0700

    build preview

--- src/screens/Splash/styles.ts
+++ src/screens/Splash/styles.ts
@@ -10,8 +10,8 @@ export const styles = StyleSheet.create({
     },
 
     logo: {
-        width: 150,
-        height: 150,
+        width: 100,
+        height: 100,
         marginBottom: 10
     },
     
