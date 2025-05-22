bfa4b37e9dc8334f2302629ee368bb1833198428
huwnghoangf <124662659+huwnghoangf@users.noreply.github.com>
2025-05-22 07:22:35 +0700

    notification_22/5_sáng

--- src/navigation/NavigationContainer.tsx
+++ src/navigation/NavigationContainer.tsx
@@ -5,6 +5,7 @@ import { navigationRef } from "./NavigationService";
 import * as Linking from 'expo-linking';
 import { AppState, AppStateStatus } from "react-native";
 import TokenService from "../services/token.service";
+import { FCM } from "../services/fcm";
 
 const AppNavigator = () => {
   const [isNavReady, setIsNavReady] = useState(false);
@@ -72,6 +73,10 @@ const AppNavigator = () => {
       ref={navigationRef}
       onReady={() => {
         setIsNavReady(true);
+        if (navigationRef.current) {
+          FCM.setNavigation(navigationRef.current);
+          FCM.handlePendingNotificationIfAny(); 
+        }
       }}
     >
       <Route />
